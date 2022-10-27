699d8246c052d68aaa116c5031f6e8ce6dd3be68
Gayatri-Lendal <gayatrilendal323@gmail.com>
2022-10-27 15:42:42 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+
+function single1_click(){
+    console.log("single click...1");
+    document.body.style.backgroundColor= 'skyblue';
+    document.body.style.color= 'white'; 
+  }
+  
+  function single2_click() {
+    console.log("single click...2");
+    document.body.style.backgroundColor = 'maroon';
+    document.body.style.color = 'black';
+  }
+  
+  function single3_click() {
+    console.log("single click...3");
+    document.body.style.backgroundColor = 'grey';
+    document.body.style.color = 'black';
+  }
+  
+  function single4_click() {
+    console.log("single click...4");
+    document.body.style.backgroundColor = 'lightgreen';
+    document.body.style.color = 'white';
+  }
+  
+  
+  function mouse_down(){
+    console.log("mouse down...");
+    document.body.style.backgroundColor= 'black';
+    document.body.style.color= 'white'; 
+  }
+  
+  function mouse_enter() {
+    console.log("mouse enter...");
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'grey';
+  }
+  
+  function mouse_up() {
+    console.log("mouse up...");
+    document.body.style.backgroundColor = 'purple';
+    document.body.style.color = 'white';
+  }
+  
+  function mouse_out() {
+    console.log("mouse out...");
+    document.body.style.backgroundColor = 'darkgreen';
+    document.body.style.color = 'white';
+  }
+  
+  function key_down() {
+    console.log("key down...");
+    document.body.style.fontFamily = 'Brush Script MT';
+    document.body.style.color = 'grey';
+  }
+  
+  function key_up() {
+    console.log("key up...");
+    document.body.style.fontFamily = 'Lucida Handwriting';
+    document.body.style.color = 'white';
+  }
+  
+  function key_press() {
+    console.log("key press...");
+    document.body.style.fontFamily = 'Copperplate';
+    document.body.style.color = 'black';
+  }
